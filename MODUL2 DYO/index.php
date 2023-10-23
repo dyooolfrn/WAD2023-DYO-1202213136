d1f627ff8d7173747e2fe29696a19704d2ba9994
dyooolfrn <akmall@student.telkomuniversity.ac.id>
2023-10-23 11:33:10 +0700

    MODUL 2

---
+++ MODUL2 DYO/index.php
@@ -0,0 +1,123 @@
+<?php
+
+// **********************  1  ************************** 
+// ========== tangkap nilai tinggi_badan dan berat_badan yang ada pada form html
+// silakan taruh code kalian di bawah
+$tinggi_badan = @($_POST['tinggi_badan']) ? $_POST['tinggi_badan']: "";
+$berat_badan = @($_POST['berat_badan']) ? $_POST['berat_badan']: "";
+// **********************  1  ************************** 
+
+
+// **********************  2  ************************** 
+// ========== buatkan sebuah perkondisian di mana 
+// tinggi_badan atau $berat_badan tidak boleh kosong nilainya, kalau kosong buatkanlah pesan error
+// silakan taruh code kalian di bawah
+if(empty($tinggi_badan) || empty($berat_badan))
+{
+    $error = "Tinggi Badan dan Berat Badan Tidak Boleh Kosong!";
+}
+
+// **********************  2  ************************** 
+
+
+// **********************  3  ************************** 
+// ========== buatkanlah perkondisian di mana Jika kesalahan Error-nya "empty", 
+// masukkan perhitungan BMI sesuai dengan rumus yang tertera pada jurnal
+// silakan taruh code kalian di bawah
+
+if(empty($error)) {
+    $tinggi_badan_meter = $tinggi_badan / 100;
+    $bmi = $berat_badan / ($tinggi_badan_meter * $tinggi_badan_meter);
+}
+
+// **********************  3  ************************** 
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalkulator BMI</title>
+    <link rel="stylesheet" href="css.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4 p-3">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title mb-4" align="center">Kalkulator BMI</h4>
+                    <h6 class="card-title mb-4" align="center">Alat untuk menghitung Indeks Massa Tubuh kamu!</h6>
+                    <form action="" method="POST">
+
+                        <div class="form-floating mb-3">
+                            <input type="number" class="form-control" name="tinggi_badan" id="tinggi_badan" placeholder="175">
+                            <label for="tinggi_badan">Tinggi Badan (CM)</label>
+                        </div>
+                        
+                        <div class="form-floating">
+                            <input type="number" class="form-control" name="berat_badan" id="berat_badan" placeholder="53">
+                            <label for="berat_badan">Berat Badan (KG)</label>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary mb-3 mt-3 w-100">Hitung</button>
+                    </form>
+
+                    <!--  **********************  4  **************************     -->
+                    <!-- Hasilnya perhitungan BMI taruh di sini yaaa!! 😊🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰 -->
+                    <!-- silakan taruh code kalian di bawah -->
+                    <?php
+                    if(isset($bmi)) {
+                        echo "<h5>Hasil Perhitungan BMI</h5>";
+                        echo "BMI Anda adalah: " . $bmi;
+                        echo "\n\n";
+
+                        if($bmi <= 18.4){
+                            echo "Status: Underweight";
+                        }elseif($bmi <= 24.9 && $bmi >= 18.5){
+                            echo "Status: Normal";
+                        }elseif($bmi <= 39.9 && $bmi >= 25.0){
+                            echo "Status: Overweight";
+                        }elseif($bmi >= 40){
+                            echo "Status: Obese";
+                        }else{
+                            echo "Status: Tidak ditemukan";
+                        }
+                    }
+                    ?>
+
+                    
+                    <!--  **********************  4  **************************     -->
+
+
+
+                    <!--  **********************  5  **************************     -->
+                    <!-- Hasil pesan error nya taruh di sini yaaa!! 😊  -->
+                    <!-- silakan taruh code kalian di bawah -->
+                    <?php
+                    if(!empty($error)) {
+                        echo "<h4>Error</h4>";
+                        echo $error;
+                    }
+                    ?>
+
+                    <!--  **********************  5  **************************     -->
+
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
