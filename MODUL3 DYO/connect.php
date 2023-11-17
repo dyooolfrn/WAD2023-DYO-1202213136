46217959fab239ecb6b515bf9b5973f73aa04766
dyooolfrn <akmall@student.telkomuniversity.ac.id>
2023-11-17 15:30:23 +0700

    modul3

---
+++ MODUL3 DYO/connect.php
@@ -0,0 +1,17 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+
+<?php
+// Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+    $koneksi = mysqli_connect('127.0.0.1:3306', 'root', '', 'modul3_wad');
+
+// 
+  
+// Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
+// check connection
+    if ($koneksi->connect_error){
+        die('Koneksi Gagal! : ' .$koneksi->connect_error);
+    }
+    echo 'Berhasil Terkoneksi!';
+// 
+?>
